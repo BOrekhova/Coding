aabb5e88ed655bf53c36f0a1c30b0758ab514545
Barbara <56512329+BOrekhova@users.noreply.github.com>
2022-04-26 15:58:34 +0300

    26/04

---
+++ cpp_notes/array.cpp
@@ -0,0 +1,9 @@
+Array 
+
+// no need library
+// one exact type of variables, you have to write the size
+
+main() {
+std::string siblings[3] = {"Ivan", "Barbara", "Sophia"}
+std::cout << sibling[1] << endl;
+}
