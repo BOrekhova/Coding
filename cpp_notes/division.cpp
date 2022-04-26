7342f5ba0698529d586fccc556714bcaf6672246
Barbara <56512329+BOrekhova@users.noreply.github.com>
2022-04-26 20:06:44 +0300

    26/04

---
+++ cpp_notes/division.cpp
@@ -0,0 +1,24 @@
+Division
+
+#include <iostream>
+
+main() 
+{
+  
+  Postitive division
+    int a, b;  // there are two positive integers
+    c = a / b; // c - the quotient
+    d = a % b; // d - the reminder is discarded
+  
+            // int / int = int
+            // int % int = int
+            // float / float = float
+            // int / float = float
+  Negative division
+    int x, y;  // there are two negative integers
+  
+  
+}
+
+
+        
