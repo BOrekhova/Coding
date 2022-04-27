14851d4a20eabd9afe76f4a8ce6802f4484c87ac
Barbara <56512329+BOrekhova@users.noreply.github.com>
2022-04-27 21:44:55 +0300

    27/04

--- cpp_notes/division.cpp
+++ cpp_notes/division.cpp
@@ -7,16 +7,22 @@ main()
   
   Postitive division
     int a, b;  // there are two positive integers
-    c = a / b; // c - the quotient
-    d = a % b; // d - the reminder is discarded
+    c = a / b; // c - the quotient (positive)
+    d = a % b; // d - the reminder is discarded (positive)
   
             // int / int = int
             // int % int = int
             // float / float = float
             // int / float = float
   Negative division
-    int x, y;  // there are two negative integers
+    int x, y;  // x - negative, y - positive
+    c = a / b; // c - the quotient (negative)
+    d = a % b; // d - the reminder is discarded (negative!!)
   
+  For the "math-right" answer you can use:
+
+    d = (a % b + b) % b
+    c = (a - d) / b
   
 }
 
