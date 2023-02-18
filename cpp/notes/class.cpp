27f5c1fa9a1fd1b2a8bb58724bb6972c6eb39b71
BOrekhova <barbaraorekhova@gmail.com>
2023-02-18 20:58:11 +0300

    change

--- cpp/notes/class.cpp
+++ cpp/notes/class.cpp
@@ -1,22 +1,53 @@
-#icnlude < iostream>
+#include <iostream>
 
-std::string RandomString(int ch)
+// class is a user-defined data type, which holds its own data members and member functions
+// do not forget the prototype of the function
+// private > protected > public
+
+////////////////////////////////
+
+// Object-oriented programming (OOP)
+// OOP = objects + classes
+// Incapsulation is the first principle of OOP
+
+////////////////////////////////
+
+class Animal {
+public:
+    int age;
+};
+
+int main()
 {
-    const int ch_MAX = 26;
-    char alpha[ch_MAX] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-        'h', 'i', 'j', 'k', 'l', 'm', 'n',
-        'o', 'p', 'q', 'r', 's', 't', 'u',
-        'v', 'w', 'x', 'y', 'z' };
-    std::string result = "";
-    for (int i = 0; i < ch; i++)
-        result = result + alpha[rand() % ch_MAX];
-
-    return result;
+    Animal Barbos;
+    Barbos.age = 15;
 }
+
+////////////////////////////////
+
+class Point {
+public:
+    int getX()
+    {
+        return x;
+    }
+
+    void setX(int x)
+    {
+        this->x = x;
+    }
+
+private:
+    int x = 0, y = 0, z = 0;
+};
+
+/////////////////////////////////
 
 class Engine {
 public:
-    Engine(int hp)
+    Engine() = default;
+
+    Engine(unsigned hp)
     {
         this->hp = hp;
     }
@@ -25,30 +56,37 @@ private:
     unsigned hp;
 };
 
-class Car_ch {
-public:
+class VehicleCharacteristics {
 protected:
     unsigned weight;
     unsigned wheelSize;
 };
 
-class Car : public Car_ch {
+class Vehicle : public VehicleCharacteristics {
 public:
-    Car(Engine engine, std::string mark, unsigned weight)
+    Vehicle(Engine engine, std::string mark, unsigned weight, unsigned wheelSize)
     {
         this->engine = engine;
         this->mark = mark;
         this->weight = weight;
+        this->wheelSize = wheelSize;
+    }
+
+    std::string getModel()
+    {
+        return mark;
     }
 
 private:
     std::string mark;
-    unsigned weight;
     Engine engine;
 };
 
+////////////////////////////////
+
 int main()
 {
-    Engine engine(500);
-    Car* car = new Car(engine, "Subaru", 5)
+    Point p1;
+    p1.setX(10);
+    std::cout << "x: " << p1.getX() << std::endl;
 }
