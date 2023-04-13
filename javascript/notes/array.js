5b1cd01fc550370dfd3edc9ceb4c98717794d54f
BOrekhova <barbaraorekhova@gmail.com>
2023-04-13 18:44:36 +0300

    add js notes about array methods

--- javascript/notes/array.js
+++ javascript/notes/array.js
@@ -8,42 +8,48 @@
 const arr1 = [];
 const arr2 = [0, '1', true, {}, undefined];
 const arr3 = [1, 2, 3];
-let str = 'Hello! How are you?';
 
 // to change an element of an array
 
-arr[index] = value;
 arr2[0] = 'Hello!';
 
 // methods and functions
 
-arr2.length; // length of an array
-
 arr2.push('el'); // add elemenet(s) to the end 
 arr2.unshift('el'); // add element(s) in the begining
-
 arr2.pop(); // delete the last element and return it
 arr2.shift(); // delete the first element and return it
+arr2.splice(0, 1) // arr2.splice(index [, deleteCount, item1, item2, item3...])
 
+const ARR1 = arr3.slice(1, 2); // ARR1 = [2]
+const ARR2 = arr3.slice(-1);  // ARR2 = [3]
+const ARR3 = arr3.slice();   // ARR3 = [1, 2, 3]
+
+const ARR4 = arr1.concat(ARR1, ARR2, ARR3); // ARR4 = [2, 3, 1, 2, 3]
+
+const ARR5 = ARR4.filter(e => e > 2); // ARR5 = [3, 3]
+
+const ARR6 = arr3.map(e => e + 1) // ARR6 = [2, 3, 4]
+
+arr2.length; // length of an array
 arr3.toString(); // array -> string // 1,2,3
 String(arr3); // array -> string // 1,2,3
 arr3.join(); // array -> string // 1,2,3
-
 arr3.join(', '); // 1, 2, 3
 str.split(); // ['Hello! How are you?']
 str.split(' '); // string -> array of strings // ["Hello!", "How", "are", "you?"]
 
 // array iteration
 
-for (let index = 0; index < array.length; index++) {
-    console.log(arr2[index]);
+for (let i=0; i<arr2.length; i++) {
+    console.log(arr2[i]);
 }
 
 for (let value of array) {
     console.log(value);
 }
 
-// MULTI ARRAYS (arrays of arrays)
+// MULTI ARRAY (array of arrays)
 
 let array = [
     [1, 2, 3],
