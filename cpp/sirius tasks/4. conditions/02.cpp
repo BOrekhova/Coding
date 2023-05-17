e77e29b07fe86b8b4bd7cdf133989d62725ae199
BOrekhova <barbaraorekhova@gmail.com>
2023-05-17 12:02:01 +0300

    solve sirius tasks

--- cpp/sirius tasks/4. conditions/02.cpp
+++ cpp/sirius tasks/4. conditions/02.cpp
@@ -4,5 +4,6 @@ int main()
 {
     int x1, y1, x2, y2;
     std::cin >> x1 >> y1 >> x2 >> y2;
-    
+    if ((x1 + y1) % 2 == (x2 + y2) % 2) std::cout << "YES";
+    else std::cout << "NO";
 }
--- cpp/sirius tasks/4. conditions/03.cpp
+++ cpp/sirius tasks/4. conditions/03.cpp
@@ -4,6 +4,6 @@ int main()
 {
     int x1, y1, x2, y2;
     std::cin >> x1 >> y1 >> x2 >> y2;
-    if ((x1 - x2 == 1 || x2 - x1 == 1) && (y1 - y2 == 2 || y2 - y1 == 2)) std::cout << "YES";
+    if (x1 == x2 || y1 == y2) std::cout << "YES";
     else std::cout << "NO";
 }
