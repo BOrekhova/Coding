c6d73a225a08c6076f155dea44f799de4f2c9551
BOrekhova <barbaraorekhova@gmail.com>
2023-07-01 20:34:29 +0300

    end

--- codeforces/solutions_python/diff_800/002waytoolongwords.py
+++
@@ -1,6 +0,0 @@
-for i in range(int(input())):
-    word = input()
-    if len(word) > 10:
-        print(word[0], (len(word)) - 2, word[-1], sep='')
-    else:
-        print(word)
--- codeforces/solutions_python/diff_800/005dominopiling.py
+++
@@ -1,2 +0,0 @@
-nums = input()
-print((int(nums[0])*int(nums[-1])) // 2)
